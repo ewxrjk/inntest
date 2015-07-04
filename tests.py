d2826c2e6a327aba619d6dbdcfaa54410abd9504
Richard Kettlewell <rjk@greenend.org.uk>
2015-07-04 16:20:33 +0100

    HTML output

--- tests.py
+++ tests.py
@@ -18,6 +18,20 @@
 import argparse,logging,re,sys,traceback
 import nntpbits,inntest
 
+class CaptureHandler(logging.Handler):
+    def __init__(self, level):
+        super().__init__(level)
+        self.records=[]
+
+    def emit(self, record):
+        self.records.append(record)
+
+    def clear(self):
+        self.records=[]
+
+def escape(s):
+    return re.sub('[&<>\"\']', (lambda m: "&#%d;" % int(ord(m.group(0)))), s)
+
 def main(argv):
     p=argparse.ArgumentParser()
     p.add_argument('-s', '--server', help='Subject news server name',
@@ -38,6 +52,8 @@ def main(argv):
     p.add_argument('TEST', help='Tests to run', nargs='*')
     p.add_argument('-L', '--list', help='List tests',
                    action='store_true')
+    p.add_argument('-H', '--html', help='HTML output',
+                   type=str, dest='HTML', default=None)
     r=p.parse_args(argv)
     logging.basicConfig(level=r.debug)
     all_tests=inntest.list_tests()
@@ -78,9 +94,45 @@ def main(argv):
     skips=[]
     fails=[]
     xfails=[]
+    capture=CaptureHandler(level=r.debug)
+    if r.HTML:
+        logging.getLogger().addHandler(capture)
+        html=open(r.HTML, "w")
+        html.write("<head>\n")
+        html.write('<link rel=stylesheet type="text/css" href="tests.css">\n')
+        html.write("<body>\n")
+        html.write("<table class=tests>\n")
+        html.write("<tr><td>test</td>\n")
+        html.write("<td>outcome</td>\n")
+        html.write("<td>xfail</td>\n")
+        html.write("<td>skip</td>\n")
+        html.write("<td>log</td>\n")
     for test_name in r.TEST:
         tested+=1
+        capture.clear()
         t_fails,t_xfails,t_skips=inntest.run_test(test_name, **args[test_name])
+        if r.HTML:
+            html.write("<tr><td class=testname>%s</td>\n" % test_name)
+            if len(t_fails):
+                html.write("<td class=fails>%d</td>\n" % len(t_fails))
+            else:
+                html.write("<td class=ok>OK</td>\n")
+            if len(t_xfails):
+                html.write("<td class=xfails>%d</td>\n" % len(t_xfails))
+            else:
+                html.write("<td class=noxfails>&nbsp;</td>\n")
+            if len(t_skips):
+                html.write("<td class=skips>%d</td>\n" % len(t_skips))
+            else:
+                html.write("<td class=noskips>&nbsp;</td>\n")
+            if len(capture.records):
+                html.write("<td class=logs><pre>")
+                for record in capture.records:
+                    html.write(escape(record.message)+"\n")
+                html.write("</pre></td>\n")
+            else:
+                html.write("<td class=nologs>&nbsp;</td>\n")
+            html.write("</tr>\n")
         fails.extend(t_fails)
         xfails.extend(t_xfails)
         skips.extend(t_skips)
@@ -96,6 +148,9 @@ def main(argv):
         logging.warn("%d expected fails" % len(xfails))
     if len(fails) > 0:
         logging.error("%d fails" % len(fails))
+    if r.HTML:
+        html.write("</table>\n")
+        html.close()
     return 1 if len(fails) > 0 else 0
 
 if __name__ == '__main__':
